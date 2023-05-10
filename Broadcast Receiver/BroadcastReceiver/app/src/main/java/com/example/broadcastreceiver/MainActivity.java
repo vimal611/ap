36e3bda70b0364d4626f8c438343136d9f8d6f7d
vimal611 <94862739+vimal611@users.noreply.github.com>
2023-05-11 00:30:56 +0530

    Add files via upload

---
+++ Broadcast Receiver/BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter=new IntentFilter("com.anything.myBroadcastMessage");
+        MyReceiver receiver=new MyReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+}
